98deeecf91c1a83c83ea1197d0c38691ef406fa1
SanjarbekUsmonov <noobpusk@gmail.com>
2022-08-25 18:05:03 +0500

    first commit

---
+++ form/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template >
+  <div>
+    <CompForm/>
+  </div>
+</template>
+<script>
+import CompForm from 'src/components/CompForm.vue';
+export default {
+    components: { CompForm }
+}
+</script>
+<style >
+
+</style>
